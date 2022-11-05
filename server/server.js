0f54cbf967693ce105d48d9e2ae1ebb285110a63
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-05 04:17:09 +0100

    Still working on server.js file

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const xss = require("xss-clean");
+const mongoSanitise = require("express-mongo-sanitize");
+
 const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri);
 
 const port = process.env.PORT || 4000;
-
 app.listen(port, () => {
   console.log(`Connected successfully on : ${port}`);
 });
