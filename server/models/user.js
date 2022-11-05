9a55ed2539453ea4560896524c6c4633321e7941
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-05 16:23:25 +0100

    Creating userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,17 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {},
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
