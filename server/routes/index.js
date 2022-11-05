674fa9a6f9b6fd548570649a14e397ef412f6022
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-05 15:06:25 +0100

    Started working on my Index.js file in router folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const routesIndex = [
+  {
+    path: "/auth",
+    route: "",
+  },
+];
+
+routesIndex.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
 module.exports = { router };
