039bbfc4f90c8b167c46e3403481d7dfe433b444
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-05 03:27:24 +0100

    created my app port

--- server/server.js
+++ server/server.js
@@ -3,3 +3,9 @@ const app = express();
 
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Connected successfully on : ${port}`);
+});
