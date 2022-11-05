56e7fd373365436d020d60a7ee4153c14f073781
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-05 04:10:20 +0100

    Created and connected my MongoDB alias to my web app

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
 
 const port = process.env.PORT || 4000;
 
