b54dbd279020a2f3bbe5fe3c02da8918931550b0
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-05 04:21:12 +0100

    Created my parser and sanitize middleware

--- server/server.js
+++ server/server.js
@@ -11,6 +11,13 @@ const mongoSanitise = require("express-mongo-sanitize");
 const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
 mongoose.connect(mongoUri);
 
+// PARSING MIDDLEWARE
+app.use(bodyParser.json());
+
+// SANITIZE MIDDLEWARE
+app.use(xss());
+app.use(mongoSanitise());
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Connected successfully on : ${port}`);
