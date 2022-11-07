9e231751f0811efd03123644464054d622ca8679
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-07 04:06:36 +0100

    Working on my passport

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -1,3 +1,12 @@
 const { User } = require("../models/user");
 require("dotenv").config();
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+
+const jwtOptions = {
+  secretOrKey: process.env.DB_SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
+const jwtVerify = async (payload, done) => {};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
