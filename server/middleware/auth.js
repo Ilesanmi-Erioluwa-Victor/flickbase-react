e52e5e2475f1930dfa22c0da5671ee4ada0d766d
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-09 16:42:19 +0100

    Working on my auth.js middleware on Promise

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,8 +2,20 @@ const passport = require("passport");
 const { ApiError } = require("../middleware/Apierror");
 const httpStatus = require("http-status");
 
+const verify = (req, res, resolve, reject) = async (err, user)=> {
+  if(err || !user) {
+    return reject(new ApiError(httpStatus.UNAUTHORIZED, "Sorry you are not authorized !!!"));
+  }
+}
+
 const auth = () => async (req, res, next) => {
-  return new Promise((resolved, reject) => {})
+  return new Promise((resolve, reject) => {
+    passport.authenticate(
+      "jwt",
+      { session: false },
+      verify(req, res, resolve, reject)(req, res, next)
+    );
+  })
     .then(() => next())
     .catch((err) => next(err));
 };
