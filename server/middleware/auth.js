184db21eca456e4589f9fae6dba186b0fb59e492
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-09 16:32:55 +0100

    Working on my auth.js middleware on Promise

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,3 +1,11 @@
 const passport = require("passport");
 const { ApiError } = require("../middleware/Apierror");
 const httpStatus = require("http-status");
+
+const auth = () => async (req, res, next) => {
+  return new Promise((resolved, reject) => {})
+    .then(() => next())
+    .catch((err) => next(err));
+};
+
+module.exports = auth;
