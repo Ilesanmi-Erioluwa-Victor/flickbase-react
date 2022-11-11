b99ff209dcda382f7d258867e375ad1935194794
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 03:13:08 +0100

    Done with updateUserProfile service

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,3 +1,6 @@
+const { ApiError } = require("../middleware/Apierror");
+const httpStatus = require("http-status"
+)
 // Models
 const { User } = require("../models/user");
 
@@ -5,29 +8,38 @@ const findUserByEmail = async (email) => {
   return User.findOne({ email });
 };
 
-const findUserById = async(_id) => {
-   return User.findById(_id)
-}
+const findUserById = async (_id) => {
+  return User.findById(_id);
+};
 
 const updateUserProfile = async (req) => {
-   try {
-     const user =  await User.findByIdAndUpdate({
-      _id : req.user._id,
-     },
-     {"$set" : {
-       firstname : req.body.firstname,
-       lastname : req.body.lastname,
-       age : req.body.age
-     }},
-     {new : true}
-     )
-   } catch (error) {
-    throw error
-   }
-}
+  try {
+    const user = await User.findByIdAndUpdate(
+      {
+        _id: req.user._id,
+      },
+      {
+        $set: {
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          age: req.body.age,
+        },
+      },
+      { new: true }
+    );
+
+    if (!user) {
+      throw new ApiError(httpStatus.NOT_FOUND, "Sorry, No user found...")
+    }
+ 
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   findUserByEmail,
   findUserById,
-  updateUserProfile
+  updateUserProfile,
 };
