4fc9e5614409bf98e9e819ebbb658ed00988a414
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-14 00:19:43 +0100

    Added express validator depencey

--- server/services/articles.service.js
+++ server/services/articles.service.js
@@ -1,23 +1,21 @@
 const httpStatus = require("http-status");
 // Models
-const { Article} = require("../models/article");
+const { Article } = require("../models/article");
 // Middleware
 const { ApiError } = require("../middleware/Apierror");
 
 const Addarticle = async (body) => {
-    try {
-         
-        const article = new Article( {
-        ...body,
-        score : parseInt(body.score)
-        })
-        await article.save();
-        return article;
-
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const article = new Article({
+      ...body,
+      score: parseInt(body.score),
+    });
+    await article.save();
+    return article;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   Addarticle,
