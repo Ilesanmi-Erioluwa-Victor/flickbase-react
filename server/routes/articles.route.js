456ab14080ee2ec765a6ebdad8f5e09896021fd6
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-14 02:04:03 +0100

    Working on delete method

--- server/routes/articles.route.js
+++ server/routes/articles.route.js
@@ -15,7 +15,8 @@ router.post(
 router
   .route("/article/:id")
   .get(auth("readAny", "articles"), articlesController.getArticleById)
-  .patch(auth("updateAny", "articles"), articlesController.updateArticleById);
+  .patch(auth("updateAny", "articles"), articlesController.updateArticleById)
+  .delete(auth("deleteAny", "articles"), articlesController.deleteArticleById);
 
 router.route("/users/article/:id").get(articlesController.getUsersArticleById);
 
