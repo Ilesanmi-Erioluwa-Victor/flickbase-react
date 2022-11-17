cb6ce6537a7793151ad032471c68b30e21993b00
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-17 18:02:29 +0100

    Created my Home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import { Home } from "./Components/Home";
 
 function App() {
   return <>
-     <Route>
-      <Route />
-     </Route>
+     <Routes>
+      <Route path="/" element={<Home />}/>
+     </Routes>
   </>;
 }
 
