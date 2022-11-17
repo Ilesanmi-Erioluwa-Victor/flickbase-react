b8890b7c612e69c83a7d0e95c13adf6b74b44a3d
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-17 18:40:03 +0100

    Working on Mui Drawer component

--- client/src/Components/Navigation/Header.js
+++ client/src/Components/Navigation/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-
+import { SideNav } from './SideNav'
 export const Header = () => {
   return (
     <nav className='navbar fixed-top'>
       <Link to="/" className='navbar-brand d-flex align-items-center fredoka_ff'>Flickbase</Link>
+      <SideNav />
     </nav>
   )
 }
