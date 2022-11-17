17888e756885d68e4cc0ac8ba8a9e6d571f3c7b1
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-17 18:45:59 +0100

    Imported all my files for Drawer

--- client/src/Components/Navigation/SideNav.js
+++ client/src/Components/Navigation/SideNav.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Drawer  from '@mui/material/Drawer';
 
 export const SideNav = () => {
   return (
