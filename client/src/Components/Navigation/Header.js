a126c88b181702f07c1e22302526c10ee00218ec
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-19 10:15:34 +0100

    Working on Mainlayout now

--- client/src/Components/Navigation/Header.js
+++ client/src/Components/Navigation/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import { SideNav } from './SideNav'
+import React from "react";
+import { Link } from "react-router-dom";
+import { SideNav } from "./SideNav";
+
 export const Header = () => {
   return (
-    <nav className='navbar fixed-top'>
-      <Link to="/" className='navbar-brand d-flex align-items-center fredoka_ff'>Flickbase</Link>
+    <nav className="navbar fixed-top">
+      <Link
+        to="/"
+        className="navbar-brand d-flex align-items-center fredoka_ff"
+      >
+        Flickbase
+      </Link>
       <SideNav />
     </nav>
-  )
-}
+  );
+};
--- client/src/Components/Navigation/SideNav.js
+++ client/src/Components/Navigation/SideNav.js
@@ -1,93 +1,90 @@
-import React, { useState } from 'react'
-import { Link as RouterLink} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Drawer  from '@mui/material/Drawer';
+import Drawer from "@mui/material/Drawer";
 
-import DehazeIcon from "@mui/icons-material/Dehaze"
-import MailIcon from "@mui/icons-material/Mail"
+import DehazeIcon from "@mui/icons-material/Dehaze";
+import MailIcon from "@mui/icons-material/Mail";
 import HomeIcon from "@mui/icons-material/Home";
 import VpnKeyIcon from "@mui/icons-material/VpnKey";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 
 export const SideNav = () => {
-
-  const [ state,  setState ] = useState(false);
+  const [state, setState] = useState(false);
 
   return (
     <>
-    <DehazeIcon className="drawer_btn" onClick = {()=>setState(true)}/>
+      <DehazeIcon className="drawer_btn" onClick={() => setState(true)} />
 
-    <Drawer anchor={"right"} open={state} onClose ={()=> setState(false)}>
-             
-             <Box sx={{width : 200}}>
-                   <List>
-                        <ListItem
-                            component={RouterLink}
-                            to="/"
-                            onClick={()=> setState(false)}
-                        >
-                            <ListItemIcon>
-                                <HomeIcon/>
-                            </ListItemIcon>
-                            <ListItemText primary="Home"/>
-                        </ListItem>
+      <Drawer anchor={"right"} open={state} onClose={() => setState(false)}>
+        <Box sx={{ width: 200 }}>
+          <List>
+            <ListItem
+              component={RouterLink}
+              to="/"
+              onClick={() => setState(false)}
+            >
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
 
-                        <ListItem
-                            component={RouterLink}
-                            to="/contact"
-                            onClick={()=> setState(false)}
-                        >
-                            <ListItemIcon>
-                                <MailIcon/>
-                            </ListItemIcon>
-                            <ListItemText primary="Contact"/>
-                        </ListItem>
+            <ListItem
+              component={RouterLink}
+              to="/contact"
+              onClick={() => setState(false)}
+            >
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary="Contact" />
+            </ListItem>
 
-                        <ListItem
-                            component={RouterLink}
-                            to="/auth"
-                            onClick={()=> setState(false)}
-                        >
-                            <ListItemIcon>
-                                <VpnKeyIcon/>
-                            </ListItemIcon>
-                            <ListItemText primary="Sign in"/>
-                        </ListItem>
+            <ListItem
+              component={RouterLink}
+              to="/auth"
+              onClick={() => setState(false)}
+            >
+              <ListItemIcon>
+                <VpnKeyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Sign in" />
+            </ListItem>
 
-                        <ListItem
-                            onClick={()=> {
-                                alert('sign out')
-                                setState(false)
-                            }}
-                        >
-                            <ListItemIcon>
-                                <VpnKeyIcon/>
-                            </ListItemIcon>
-                            <ListItemText primary="Sign out"/>
-                        </ListItem>
+            <ListItem
+              onClick={() => {
+                alert("sign out");
+                setState(false);
+              }}
+            >
+              <ListItemIcon>
+                <VpnKeyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Sign out" />
+            </ListItem>
 
-                        <>
-                            <Divider/>
-                            <ListItem
-  
-                               component={RouterLink}
-                               to="/dashboard"
-                               onClick={()=> setState(false)}
-                            >
-                                <ListItemIcon>
-                                    <DashboardIcon/>
-                                </ListItemIcon>
-                                <ListItemText primary="Dashboard"/>
-                            </ListItem>
-                        </>
-                    </List>
-             </Box>
-    </Drawer>
+            <>
+              <Divider />
+              <ListItem
+                component={RouterLink}
+                to="/dashboard"
+                onClick={() => setState(false)}
+              >
+                <ListItemIcon>
+                  <DashboardIcon />
+                </ListItemIcon>
+                <ListItemText primary="Dashboard" />
+              </ListItem>
+            </>
+          </List>
+        </Box>
+      </Drawer>
     </>
-  )
-}
+  );
+};
