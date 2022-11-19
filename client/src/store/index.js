818054818584907e9056e6e1e5f801ca3f705e29
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-19 10:43:06 +0100

    import my reducers to store reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import UsersReducer from "./reducers/users"
+
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    users : UsersReducer
+  },
 });
--- client/src/store/reducers/users.js
+++ client/src/store/reducers/users.js
@@ -1,17 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
+// Initial state before fetching api for users
 let DEFAULT_STATE = {
-    loading : false,
-    data : {
-        _id : null,
-        email : null,
-        firstname : null,
-        lastname : null
-    }
-}
+  loading: false,
+  data: {
+    _id: null,
+    email: null,
+    firstname: null,
+    lastname: null,
+    age: null,
+    role: null,
+    verified: null,
+  },
+  auth: null,
+};
 
 export const usersSlice = createSlice({
-    name : "users",
-    initialState : {}
-})
+  name: "users",
+  initialState: DEFAULT_STATE,
+
+  reducers : {
+
+  }
+//   End of reducers object.....
+
+});
+
+export default usersSlice.reducer
