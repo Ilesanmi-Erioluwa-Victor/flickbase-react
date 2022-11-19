d583aa2d42c0582dd277249928c16f146f10e593
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-19 10:56:17 +0100

    Created my Notitications reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UsersReducer from "./reducers/users"
+import ArticlesReducer from "./reducers/articles";
+import NotificationsReducer from "./reducers/notifications";
 
 
 export const store = configureStore({
   reducer: {
-    users : UsersReducer
+    users : UsersReducer,
+    articles : ArticlesReducer,
+    notifications : NotificationsReducer
   },
+  
 });
---
+++ client/src/store/reducers/notifications.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const notificationsSlice = createSlice({
+  name: "notifications",
+  initialState: {
+   global : {}
+  },
+
+  reducers: {},
+  //   End of reducers object.....
+});
+
+export default notificationsSlice.reducer;
