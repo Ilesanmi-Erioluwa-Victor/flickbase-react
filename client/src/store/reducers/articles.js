41f3919d5719ee65244581e859f3f7e82fc28f54
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-19 10:47:30 +0100

    Created my articles reducers

---
+++ client/src/store/reducers/articles.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const articlesSlice = createSlice({
+  name: "articles",
+  initialState:  {
+    homeSort : {},
+    loading : false,
+    articles : [],
+    current : null
+  },
+
+  reducers: {},
+  //   End of reducers object.....
+});
+
+export default articlesSlice.reducer;
