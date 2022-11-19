16d1b49b3b24ef2270319f21ec732c68faee9e31
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-19 11:24:51 +0100

    Working with formik

--- client/src/Components/Auth/index.js
+++ client/src/Components/Auth/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
-import * as yup from "yup";
+import * as Yup from "yup";
 
 import { useDispatch, useSelector } from "react-redux";
 import  Box  from "@mui/material/Box";
@@ -9,5 +9,19 @@ import  Button  from "@mui/material/Butt";
 
 
 export const Auth = () => {
+    //   Check for users registering or logining...
+    const [register, setRegister ] = useState(false);
+
+    // creating inputs using formik..
+    const formik = useFormik({
+     initialValues : {
+        emaiil : "",
+        password : ""
+     },
+     validationSchema : Yup.object({
+        // Validator.....
+        email : Yup.string().required("Sorry, kindly provide email,cause, it's important")
+     })
+    })
   return <div>AUTH .....</div>;
 };
