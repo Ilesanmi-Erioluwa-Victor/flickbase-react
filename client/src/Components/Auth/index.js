d3c9874692fd873b655427fd571d4dbd71ea66eb
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-19 11:18:28 +0100

    Importing all the necessary indepencies

--- client/src/Components/Auth/index.js
+++ client/src/Components/Auth/index.js
@@ -1,9 +1,13 @@
-import React, {useState, useEffect } from 'react'
-import { useFormik } from "formik"
-import * as yup from "yup"
+import React, { useState, useEffect } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+import { useDispatch, useSelector } from "react-redux";
+import  Box  from "@mui/material/Box";
+import  TextField  from "@mui/material/TextField";
+import  Button  from "@mui/material/Butt";
+
 
 export const Auth = () => {
-  return (
-    <div>AUTH .....</div>
-  )
-}
+  return <div>AUTH .....</div>;
+};
