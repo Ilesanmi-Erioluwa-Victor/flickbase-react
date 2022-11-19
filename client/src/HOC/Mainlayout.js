221f29754d57f16cfc6fe67332d16da1271b46c9
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-19 10:20:00 +0100

    Working on layout

--- client/src/HOC/Mainlayout.js
+++ client/src/HOC/Mainlayout.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import { ToastContainer } from "react-toastify";
 
-export const Mainlayout = () => {
-  return (
-    <div>Mainlayout</div>
-  )
-}
+export const Mainlayout = ({ children }) => {
+  return <Container className={`app_container mb-5`}>{children}</Container>;
+};
