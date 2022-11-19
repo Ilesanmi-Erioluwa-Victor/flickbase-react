d09f068bdbf754114053dcd16dfc4ae1f33aaf88
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-19 10:32:40 +0100

    Working on store file

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,15 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import { store } from "./store"
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
