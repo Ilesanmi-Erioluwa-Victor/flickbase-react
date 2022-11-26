a2ddc65b9dff10dbccf7044b14bbbc585b325e28
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-26 18:28:21 +0100

    Working on add articles route

--- client/src/Components/Dashboard/articles/edit_add/Add.js
+++ client/src/Components/Dashboard/articles/edit_add/Add.js
@@ -7,6 +7,23 @@ import { errorHelper, Loader} from '../../../../Utils';
 // redux
 import { useSelector, useDispatch } from 'react-redux';
 
+// MUI
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Chip from "@mui/material/Chip";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+
+import InputLabel from "@mui/material/InputLabel";
+import AddIcon from "@mui/icons-material/Add";
+import { visuallyHidden } from "@mui/utils";
+
 import { AdminTitle } from '../../../../Utils'
 
 export const AddArticles = () => {
