84782f9f2a7a1d885fa5e4933eac9de1ab1d0dbb
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-26 18:11:59 +0100

    Working on add articles

---
+++ client/src/Components/Dashboard/articles/edit_add/Add.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AdminTitle } from '../../../../Utils'
+
+export const addArticles = () => {
+  return (
+    <>
+      <>
+        <AdminTitle title="Add articles" />
+      </>
+    </>
+  );
+}
